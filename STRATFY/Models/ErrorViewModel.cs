64f00d7171c26a5e680a1f6ccf3dcbd629faa916
Gabriel Collopy <gabrielcollopy96@gmail.com>
2025-02-22 17:50:43 -0300

    Add project files.

---
+++ STRATFY/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace STRATFY.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
