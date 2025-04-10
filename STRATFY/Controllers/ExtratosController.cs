b950e0608733e4db91826652521c253b62208e81
Gabriel Collopy <gabrielcollopy96@gmail.com>
2025-04-10 11:34:30 -0300

    tela de edit de extrato modificada

--- STRATFY/Controllers/ExtratosController.cs
+++ STRATFY/Controllers/ExtratosController.cs
@@ -9,6 +9,7 @@ using STRATFY.Models;
 using STRATFY.Interfaces;
 using Microsoft.VisualBasic;
 using Microsoft.AspNetCore.Authorization;
+using System.Security.Cryptography.Xml;
 
 namespace STRATFY.Controllers
 {
@@ -80,50 +81,140 @@ namespace STRATFY.Controllers
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
-            {
                 return NotFound();
+
+            var extrato = await _context.Extratos
+                .Include(e => e.Movimentacaos)
+                .FirstOrDefaultAsync(e => e.Id == id);
+
+            if (extrato == null)
+                return NotFound();
+
+            var viewModel = new ExtratoEdicaoViewModel
+            {
+                ExtratoId = extrato.Id,
+                NomeExtrato = extrato.Nome,
+                DataCriacao = extrato.DataCriacao,
+                Movimentacoes = extrato.Movimentacaos.Select(m => new Movimentacao
+                {
+                    Id = m.Id,
+                    Descricao = m.Descricao,
+                    Valor = m.Valor,
+                    Tipo = m.Tipo,
+                    CategoriaId = m.CategoriaId,
+                    ExtratoId = m.ExtratoId
+                }).ToList()
+            };
+
+            ViewData["CategoriaId"] = new SelectList(await _context.Categoria.ToListAsync(), "Id", "Nome");
+
+            return View(viewModel);
+        }
+
+        //if (id == null)
+        //{
+        //    return NotFound();
+        //}
+
+        //var extrato = await _context.Extratos.FindAsync(id);
+        //if (extrato == null)
+        //{
+        //    return NotFound();
+        //}
+        //ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Id", extrato.UsuarioId);
+        //return View(extrato);
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ExtratoEdicaoViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewData["CategoriaId"] = new SelectList(await _context.Categoria.ToListAsync(), "Id", "Nome");
+                return View(model);
             }
 
-            var extrato = await _context.Extratos.FindAsync(id);
+            var extrato = await _context.Extratos
+                .Include(e => e.Movimentacaos)
+                .FirstOrDefaultAsync(e => e.Id == model.ExtratoId);
+
             if (extrato == null)
             {
                 return NotFound();
             }
-            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Id", extrato.UsuarioId);
-            return View(extrato);
+
+            // Atualiza os dados do extrato
+            extrato.Nome = model.NomeExtrato;
+            extrato.DataCriacao = model.DataCriacao;
+
+            // Lista de IDs recebidos do form
+            var idsRecebidos = model.Movimentacoes.Select(m => m.Id).ToList();
+
+            // REMOVE movimentações que existiam no banco mas não vieram no form
+            var movimentacoesRemovidas = extrato.Movimentacaos
+                .Where(m => !idsRecebidos.Contains(m.Id))
+                .ToList();
+
+            _context.Movimentacaos.RemoveRange(movimentacoesRemovidas);
+
+            // Itera sobre cada movimentação recebida no form
+            foreach (var mov in model.Movimentacoes)
+            {
+                if (mov.Id == 0)
+                {
+                    // Nova movimentação
+                    mov.ExtratoId = model.ExtratoId;
+                    _context.Movimentacaos.Add(mov);
+                }
+                else
+                {
+                    // Atualização
+                    var movBanco = extrato.Movimentacaos.FirstOrDefault(m => m.Id == mov.Id);
+                    if (movBanco != null)
+                    {
+                        movBanco.Descricao = mov.Descricao;
+                        movBanco.Valor = mov.Valor;
+                        movBanco.Tipo = mov.Tipo;
+                        movBanco.CategoriaId = mov.CategoriaId;
+                    }
+                }
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
-        // POST: Extratos/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit( Extrato extrato)
-        {
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    extrato.DataCriacao = DateOnly.FromDateTime(DateTime.Now);
-                    _context.Update(extrato);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ExtratoExists(extrato.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Id", extrato.UsuarioId);
-            return View(extrato);
-        }
+
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit( Extrato extrato)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            extrato.DataCriacao = DateOnly.FromDateTime(DateTime.Now);
+        //            _context.Update(extrato);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!ExtratoExists(extrato.Id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Id", extrato.UsuarioId);
+        //    return View(extrato);
+        //}
 
         // GET: Extratos/Delete/5
         public async Task<IActionResult> Delete(int? id)
---
+++ STRATFY/Models/ExtratoEdicaoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace STRATFY.Models
+{
+    public class ExtratoEdicaoViewModel
+    {
+        public int ExtratoId { get; set; }
+        public string NomeExtrato { get; set; } = string.Empty;
+        public DateOnly DataCriacao { get; set; }
+
+        public List<Movimentacao> Movimentacoes { get; set; } = new List<Movimentacao>();
+    }
+}
